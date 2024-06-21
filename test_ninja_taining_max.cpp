8a8eaba51d99c655649843937f2d46ea608527da
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-06-22 03:36:21 +0530

    dp ninja problem

--- test_ninja_taining_max.cpp
+++ test_ninja_taining_max.cpp
@@ -1,53 +1,116 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(vector<int> arr, vector<int> dp, int n)
+// int helper(int n, vector<vector<int>> arr, int i, int last) {
+
+//   if (i == 0) {
+//     int temp = 0;
+//     for (int j = 0; j < 3; j++) {
+//       if (j != last) {
+//         temp = max(arr[i][j], temp);
+//       }
+//     }
+//     return temp;
+//   }
+//   int temp = 0;
+//   for (int j = 0; j < 3; j++) {
+//     if (j != last) {
+
+//       int temp1 = arr[i][j] + helper(n, arr, i - 1, j);
+//       temp = max(temp1, temp);
+//     }
+//   }
+//   return temp;
+// }
+
+// int helper(int n, vector<vector<int>> &arr, vector<vector<int>> &dp, int i,
+//            int last) {
+
+//   if (i == 0) {
+//     int temp = 0;
+//     for (int j = 0; j < 3; j++) {
+//       if (j != last) {
+//         temp = max(arr[i][j], temp);
+//       }
+//     }
+//     return temp;
+//   }
+//   if (dp[i][last] != -1) {
+
+//     return dp[i][last];
+//   }
+//   int temp = 0;
+//   for (int j = 0; j < 3; j++) {
+//     if (j != last) {
+
+//       int temp1 = arr[i][j] + helper(n, arr, dp, i - 1, j);
+//       temp = max(temp1, temp);
+//     }
+//   }
+//   dp[i][last] = temp;
+//   return temp;
+// }
+// int helper(int n, vector<vector<int>> &arr, int i, int last) {
+
+//   vector<vector<int>> dp(n + 1, vector<int>(4, 0));
+//   dp[0][0] = max(arr[0][1], arr[0][2]);
+//   dp[0][1] = max(arr[0][0], arr[0][2]);
+//   dp[0][2] = max(arr[0][1], arr[0][0]);
+//   dp[0][3] = max({arr[0][1], arr[0][1], arr[0][2]});
+//   for (int i = 1; i < n; i++) {
+//     for (int k = 0; k < 4; k++) {
+//       dp[i][k] = 0;
+
+//       for (int j = 0; j < 3; j++) {
+//         if (j != k) {
+
+//           int temp = arr[i][j] + dp[i - 1][j];
+
+//               dp[i][k] = max(dp[i][k], temp);
+//         }
+//       }
+//     }
+//   }
+
+//   return dp[n-1][3];
+// }
+
+int helper(int n, vector<vector<int>> &arr, int i, int last)
 {
 
-    int temp1 = 0;
-    int temp2 = arr[0];
-    for (int i = 1; i < n - 1; i++)
+    vector<vector<int>> dp(n + 1, vector<int>(4, 0));
+    vector<int> prev(4, 0);
+    vector<int> curr(4, 0);
+    prev[0] = max(arr[0][1], arr[0][2]);
+    prev[1] = max(arr[0][0], arr[0][2]);
+    prev[2] = max(arr[0][1], arr[0][0]);
+    prev[3] = max({arr[0][1], arr[0][1], arr[0][2]});
+    for (int i = 1; i < n; i++)
     {
-
-        int inc = temp1 + arr[i];
-        int exe = temp2 + 0;
-        int ans = max(inc, exe);
-        temp1 = temp2;
-        temp2 = ans;
-    }
-    return temp2;
-}
-
-void func()
-{
-    vector<int> arr = {3, 2, 7, 10};
-    int n = arr.size();
-    int res = 0;
-
-    int i = 0;
-    vector<int> dp(n, 0);
-    vector<int> arr1;
-    vector<int> arr2;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (i != 0)
+        for (int k = 0; k < 4; k++)
         {
-            arr1.push_back(arr[i]);
+            curr[k] = 0;
+
+            for (int j = 0; j < 3; j++)
+            {
+                if (j != k)
+                {
+
+                    int temp = arr[i][j] + prev[j];
+
+                    curr[k] = max(curr[k], temp);
+                }
+            }
         }
-        if (i != n - 1)
-        {
-            arr2.push_back(arr[i]);
-        }
+        prev = curr;
     }
 
-    res = max(helper(arr1, dp, n), helper(arr2, dp, n));
-    cout << res;
+    return curr[3];
 }
-
-int main()
+int ninjaTraining(int n, vector<vector<int>> &points)
 {
-    func();
-    return 0;
+    vector<vector<int>> dp(n, vector<int>(4, -1));
+    int res = helper(n, points, n - 1, 3);
+
+    return res;
 }
