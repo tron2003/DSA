c31fe84af0e46bf026ed4c03d6051597d1c8b187
Kanish <kanishm245@gmail.com>
2024-10-20 04:20:19 +0530

    longest palidrome substring

---
+++ ongest-palindromic-substring.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        if (s.empty())
+        {
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            int odd = expand(s, i, i);
+            int even = expand(s, i, i + 1);
+            int max_len = max(odd, even);
+
+            if (max_len > end - start)
+            {
+                start = i - (max_len - 1) / 2;
+                end = i + max_len / 2;
+            }
+        }
+
+        return s.substr(start, end - start + 1);
+    }
+
+private:
+    int expand(string s, int left, int right)
+    {
+        while (left >= 0 && right < s.length() && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+};
